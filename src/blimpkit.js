658fee0e8310c391070cb1217fc72284a801895e
StanZGenchev <stan.z.genchev@gmail.com>
2026-02-16 10:54:55 +0200

    Updated version
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>

--- src/blimpkit.js
+++ src/blimpkit.js
@@ -11,7 +11,7 @@
  */
 const blimpkit = angular
   .module('blimpKit', ['ngAria'])
-  .info({ version: '1.8.4' })
+  .info({ version: '1.8.5' })
   .constant('ScreenEdgeMargin', {
     FULL: 16,
     DOUBLE: 32,
