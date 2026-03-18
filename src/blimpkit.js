1e8332e42b7d4806050043ba5ca671dcb28db945
StanZGenchev <stan.genchev@codbex.com>
2026-03-18 22:38:20 +0200

    Fixed group header bg, version 1.8.9
    Signed-off-by: StanZGenchev <stan.genchev@codbex.com>

--- src/blimpkit.js
+++ src/blimpkit.js
@@ -11,7 +11,7 @@
  */
 const blimpkit = angular
   .module('blimpKit', ['ngAria'])
-  .info({ version: '1.8.8' })
+  .info({ version: '1.8.9' })
   .constant('ScreenEdgeMargin', {
     FULL: 16,
     DOUBLE: 32,
