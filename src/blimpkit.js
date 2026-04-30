ca5340c9b5bd62c97e9579a607a29f795eecc0e1
StanZGenchev <stan.genchev@codbex.com>
2026-04-30 12:54:16 +0300

    Popover arrow color fix, vnav last item border radius fix
    Signed-off-by: StanZGenchev <stan.genchev@codbex.com>

--- src/blimpkit.js
+++ src/blimpkit.js
@@ -11,7 +11,7 @@
  */
 const blimpkit = angular
   .module('blimpKit', ['ngAria'])
-  .info({ version: '2.1.4' })
+  .info({ version: '2.1.5' })
   .constant('ScreenEdgeMargin', {
     FULL: 16,
     DOUBLE: 32,
