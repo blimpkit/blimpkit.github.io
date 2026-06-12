6c222c1af18cdc71df0f9c15215e0648976ae50c
StanZGenchev <stan.genchev@codbex.com>
2026-06-12 23:20:56 +0300

    Fixed light theme object header shadow
    Signed-off-by: StanZGenchev <stan.genchev@codbex.com>

--- src/blimpkit.js
+++ src/blimpkit.js
@@ -11,7 +11,7 @@
  */
 const blimpkit = angular
   .module('blimpKit', ['ngAria'])
-  .info({ version: '2.3.0' })
+  .info({ version: '2.3.1' })
   .constant('ScreenEdgeMargin', {
     FULL: 16,
     DOUBLE: 32,
